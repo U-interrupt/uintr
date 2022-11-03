c8c0e673a29632bf484e482df5d35f1bc88c6e37
TKF <kaifu6821@qq.com>
2022-11-03 22:50:58 +0800

    Update docs and system_top.v. Confused: AXI id bits are truncated in address_mapper.v

--- board/zcu102/rtl/system_top.v
+++ board/zcu102/rtl/system_top.v
@@ -4,9 +4,27 @@ module uintr_system_top (
     output [7:0] led
 );
 
-`axi_wire(AXI_MEM_MAPPED, 64, 1)
-`axi_wire(AXI_MEM, 64, 1)
-`axi_wire(AXI_MMIO, 64, 8)
+`axi_wire(AXI_MEM_MAPPED, 64, 4)
+`axi_wire(AXI_MEM, 64, 4);
+`axi_wire(AXI_MMIO, 64, 4);
+`axi_wire(AXI_DMA, 64, 2);
+
+wire core_clk, core_rstn;
+wire mm2s_introut, s2mm_introut;
+wire [4:0] core_uart_irq;
+
+soc zynq_soc0 (
+    `axi_connect_if(S_AXI_MEM, AXI_MEM_MAPPED),
+    `axi_connect_if(S_AXI_MMIO, AXI_MMIO),
+    `axi_connect_if(M_AXI_DMA, AXI_DMA),
+
+    .mm2s_introut       (mm2s_introut),
+    .s2mm_introut       (s2mm_introut),
+
+    .core_clk           (core_clk),
+    .core_rstn          (core_rstn),
+    .core_uart_irq      (core_uart_irq)
+);
 
 
 
